e41f4497e768a2cee816e1c93b0280ce35b092a0
lebohangkhaeane <khaeane@thedigitalacademy.co.za>
2022-10-06 13:39:14 +0200

    Initialise components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import ME from "../../assets/myimage.jpeg";
+import Socials from './Socials';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Lebohang Khaeane</h1>
+        <h5 className="text-light">Full stack Developer</h5>
+        <CTA />
+        <Socials />
+        <div className="me">
+          <img src={ME} alt="ME" />
+        </div>
+
+        <a href='#contact' className='scroll'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/header/Socials.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+
+const Socials = () => {
+    return (
+        <div className='header_socials'>
+            <a href='linkedin.com/in/lebohang-khaeane-03a388192' target="_blank"><BsLinkedin /></a>
+            <a href='https://github.com/lebotreasure' target="_blank"><FaGithub /></a>
+        </div>
+    )
+}
+
+export default Socials
