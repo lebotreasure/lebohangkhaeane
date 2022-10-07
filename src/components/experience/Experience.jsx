e881d7b57432497eec97df5b8d03e4677938b64e
lebohangkhaeane <khaeane@thedigitalacademy.co.za>
2022-10-07 15:03:42 +0200

    Experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,115 @@
 import React from 'react';
 import './experience.css';
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
     <section id="experience">
       <h5>Skills I have</h5>
       <h2>My Experience</h2>
-      
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frondend Development</h3>
+          <div className="experience_content">
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>CSS/SCSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>Angular</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>NextJS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience_backend">
+          <h3>Frondend Development</h3>
+          <div className="experience_content">
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>NodeJS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>Express</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience-details'>
+              <BsFillPatchCheckFill />
+              <div>
+                <h4>Restful API</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
